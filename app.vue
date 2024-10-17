4d140d88fecf16170b50c210081bcf07879da3f9
SergioIgnatev <ignatevsergey17@gmail.com>
2024-10-17 17:09:37 +0300

    allowEmpty

--- app.vue
+++ app.vue
@@ -1,19 +1,13 @@
 <template>
-  <div class="px-5">
- <div v-for="b in data" :key="b.id">
-<h1 class="text-[30px] font-extrabold"><a target="blanc" :href="`https://www.klerk.ru${b.url}`">{{ b.title }}</a></h1>
-
-<section v-for="child in b.children">
-<h1 class="text-[20px] font-bold"><a target="blanc" :href="`https://www.klerk.ru${child.url}`">{{ child.title }}</a></h1>
-<p>{{ child.count }}</p>
-
-</section>
- </div>
+  <div>
+    <TheChild :swi="definer" />
+    <button @click="definer=!definer">SWITCH</button>
+    <NuxtPage/>
   </div>
 </template>
 
 <script  setup>
-const {data}= await useFetch('https://www.klerk.ru/yindex.php/v3/event/rubrics')
+const definer=ref(false)
 </script>
 
 <style>
