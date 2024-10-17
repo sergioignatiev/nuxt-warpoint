aac3ab12c0f4d44a02cc19b65378e34464c47758
SergioIgnatev <ignatevsergey17@gmail.com>
2024-10-17 17:39:09 +0300

    first child

--- components/TheChild.vue
+++ components/TheChild.vue
@@ -1,11 +1,15 @@
 <template>
-  <div>
-   
-  </div>
+ <section>
+    <a :href="`https://www.klerk.ru${url}`" target="blanc"><h2 class="text-[20px] font-bold">{{ title }}</h2></a>
+   <div>{{ count }}</div>
+
+
+
+</section>
 </template>
 
 <script setup>
-
+defineProps(['url','title','count'])
 </script>
 
 <style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,21 @@
 <template>
   <div class="px-5">
-<div class="text-3xl font-extrabold">{{ total }}</div>
+
     <div v-for="b in data" :key="b.id">
 <h1 class="text-[30px] font-extrabold"><a target="blanc" :href="`https://www.klerk.ru${b.url}`">{{ b.id }} {{ b.title }}</a></h1>
+<TheChild v-for="c in b.children" 
+:title="c.title"
+:url="c.url"
+:count="c.count"
+:key="c.id"/>
 
-<section v-for="child in b.children">
-<h1 class="text-[20px] font-bold"><a target="blanc" :href="`https://www.klerk.ru${child.url}`"><span>{{ child.id }}</span> {{ child.title }}</a></h1>
-<div class=""><input type="checkbox" @click="addToTotal(child.count)">{{ child.count }}</div>
-
-</section>
  </div>
   </div>
 </template>
 
 <script  setup>
 const {data}= await useFetch('https://www.klerk.ru/yindex.php/v3/event/rubrics')
-const total=ref(0)
-function addToTotal(x){
-  total.value+=x
-}
+
 </script>
 
 <style>
