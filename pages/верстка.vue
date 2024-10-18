0f02158b666160f967b23eb5b67e2d355eea26d3
SergioIgnatev <ignatevsergey17@gmail.com>
2024-10-18 17:23:29 +0300

    clerc

---
+++ pages/верстка.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div class="flex items-center justify-center pt-[48px] pb-[28px]">
+ <img class="w-[76px]" src="/public/klerk.png" alt="clerk">
+ </div>
+ <div class="flex justify-center text-[#999999] text-[13px] font-normal">Хлебные крошки &#8594 Хлебные Крошки &#8594 Хлебные крошки </div>
+  <h1 class="bg-red-100 text-center text-[56px] font-medium">Подписки «Клерка»</h1>
+  
+  </div>
+</template>
+
+<script  setup>
+
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+*{
+    font-family: "Roboto", sans-serif;
+}
+</style>
